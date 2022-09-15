974b79dbe94f8e3ccefbac930ed18ce16992defa
jnorthr <james.b.northrop@googlemail.com>
2022-09-15 17:46:42 +0100

    Revise jvm code for new destination
    fix sample files to include fake martin quickman within the file structure

---
+++ src/main/groovy/com/toolkit/tools/Dialog.groovy
@@ -0,0 +1,343 @@
+package com.toolkit.tools;
+
+import groovy.swing.SwingBuilder  
+import groovy.beans.Bindable  
+import static javax.swing.JFrame.EXIT_ON_CLOSE  
+import java.awt.*
+import groovy.transform.*;
+import com.toolkit.tools.ProjectProperties;
+/*	
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** 
+ * Dialog class description
+ *
+ * This is code with all bits needed to do a project
+
+ *
+ */ 
+ @Canonical 
+ public class Dialog
+ {
+    /** an O/S specific char. as a file path divider */
+    String fs = java.io.File.separator;
+
+
+    /** an O/S non-specific object to host a GUI */ 
+    SwingBuilder swingBuilder = new SwingBuilder()
+    
+   /** 
+    * Variable name of current class.
+    */  
+    String classname = "Dialog";
+
+    //@groovy.beans.Bindable
+	ProjectProperties address = new ProjectProperties();
+
+   /** 
+    * Default Constructor 
+    * 
+    * @return Dialog object
+    */     
+    public Dialog()
+    {
+        classname = "running Dialog constructor"
+    } // end of constructor
+
+
+   /** 
+    * Non-Default Constructor 
+    * 
+    * @param projectDir tells us actual folder our project is running from
+    * @return Dialog object
+    */     
+    public Dialog(String projectDir)
+    {
+        classname = "running non-dft Dialog constructor"
+        address = new ProjectProperties(projectDir);
+    } // end of constructor
+
+
+   /** 
+    * Method to display internal variables.
+    * 
+    * @return formatted content of internal variables
+    */     
+    public String toString()
+    {
+        return """classname=${classname}
+"""
+    }  // end of string
+
+   /** 
+    * Method to print audit log.
+    * 
+    * @param the text to be said
+    * @return void
+    */     
+    public void say(txt)
+    {
+        println txt;
+    }  // end of method
+
+
+   /** 
+    * Method to build GUI.
+    * 
+    * @return void
+    */     
+    public void build()
+    {
+		// edt method makes sure UI is build on Event Dispatch Thread.
+		swingBuilder.edt 
+		{   
+		// Simple change in look and feel where lookAndFeel is 'nimbus'  
+	    frame(title: 'Project Properties', size: [500, 550], minimumSize: [450, 500], show: true, locationRelativeTo: null, defaultCloseOperation: EXIT_ON_CLOSE) 
+    	{ 
+        	borderLayout(vgap: 10)
+        	panel(constraints: BorderLayout.CENTER, minimumSize:[450, 500], border: compoundBorder([emptyBorder(10), titledBorder('Enter your values:') ]) ) 
+        	{
+            	tableLayout {
+                	tr {
+						td {
+                	        label 'Git User:'  // text property is default, so it is implicit.
+                    	}
+                    	td {
+                        	textField address.gitHubUser, id: 'gitHubUser', columns: 20
+                    	}
+                	} // end of tr
+
+	                tr {
+    	                td {
+    	                    label 'Author:'
+    	                }
+    	                td {
+    	                    textField id: 'author', columns: 20, address.author
+    	                }
+    	            } // end of tr
+
+	                tr {
+    	                td {
+    	                    label 'Email Addr :'
+    	                }
+    	                td {
+    	                    textField id: 'email', columns: 20, address.email
+    	                }
+    	            } // end of tr
+                
+	                tr {
+    	                td {
+    	                    label 'Year:'
+    	                }
+    	                td {
+    	                    textField id: 'year', columns: 5, text: address.year
+    	                }
+    	            } // end of tr
+
+	                tr {
+    	                td {
+    	                    label ' '
+    	                }
+    	                td {
+    	                    label ' '
+    	                }
+    	            } // end of tr
+
+	                tr {
+    	                td {
+    	                    label 'Following Values '
+    	                }
+    	                td {
+    	                    label 'Are Physical Folder Locations '
+    	                }
+    	            } // end of tr
+
+	                tr {
+    	                td {
+    	                    label 'Root Folder:'
+    	                }
+    	                td {
+    	                    textField id: 'pr', columns: 30, address.projectRoot
+    	                }
+    	            } // end of tr
+
+	                tr {
+    	                td {
+    	                    label 'Project Folder:'
+    	                }
+    	                td {
+    	                    textField id: 'projectName', columns: 20, address.projectName
+    	                }
+    	            } // end of tr                                
+
+	                tr {
+    	                td {
+    	                    label 'Project Location:'
+    	                }
+    	                td {
+    	                    textField id: 'projectLocation', columns: 25, address.projectLocation
+    	                }
+    	            } // end of tr                                
+
+                tr {
+                    td {
+                        label ' '
+                    }
+                    td {
+                        label ' '
+                    }
+                } // end of tr
+
+
+                tr {
+                    td {
+                        label 'Following Values '
+                    }
+                    td {
+                        label 'Inserted Into Generated Source '
+                    }
+                } // end of tr
+
+                                                                
+                tr {
+                    td {
+                        label 'Src Folder :'
+                    }
+                    td {
+                        textField id: 'packageFolder', columns: 25, address.packageFolder
+                    }
+                } // end of tr
+                
+                tr {
+                    td {
+                        label 'Package :'
+                    }
+                    td {
+                        textField id: 'packageName', columns: 25, address.packageName
+                    }
+                } // end of tr
+
+                tr {
+                    td {
+                        label 'Src Pkg Prefix :'
+                    }
+                    td {
+                        textField id: 'packagePrefix', columns: 25, address.packagePrefix
+                    }
+                } // end of tr
+                
+
+
+                tr {
+                    td {
+                        label ' '
+                    }
+                    td {
+                        label ' '
+                    }
+                } // end of tr
+
+                tr {
+                    td {
+                        label 'Following Values '
+                    }
+                    td {
+                        label 'Generate Source Code Classes'
+                    }
+                } // end of tr
+                
+                tr {
+                    td {
+                        label 'First Class :'
+                    }
+                    td {
+                        textField id: 'cn', columns: 25, address.primaryClassName
+                    }
+                } // end of tr
+                
+                tr {
+                    td {
+                        label 'More Classes :'
+                    }
+                    td {
+                        textField id: 'cs', columns: 25, address.classes
+                    }
+                } // end of tr
+                
+            } // end of tableLayout            
+        } // end of panel
+
+        panel(constraints: BorderLayout.SOUTH,maximumSize:[250, 50],background:Color.WHITE ) {
+            hbox{
+                button text: 'O k', actionPerformed: { System.exit(0); }
+                button text: 'Save', actionPerformed: { address.dump(); exit.requestFocus();}
+                button id: 'exit', text: 'Exit', actionPerformed: { System.exit(0); }
+            } // end of hbox
+        } // end of panel
+        
+        
+        // Binding of textfield's to address object.
+        bean address, 
+            gitHubUser: bind { gitHubUser.text }, 
+            author: bind { author.text },
+            email: bind { email.text },
+            year: bind { year.text }, 
+
+            projectRoot: bind { pr.text },
+            projectName: bind { projectName.text },
+            projectLocation: bind { projectLocation.text },
+
+            packageName: bind { packageName.text },
+            packageFolder: bind { packageFolder.text },
+            packagePrefix: bind { packagePrefix.text },
+            
+            primaryClassName: bind { cn.text },
+            classes: bind { cs.text }
+        } // end of frame  
+	  } // end of swingBuilder.edt
+
+    }  // end of method
+
+
+
+   // ======================================
+   /** 
+    * Method to run class tests.
+    * 
+    * @param args Value is string array - possibly empty - of command-line values. 
+    * @return void
+    */     
+    public static void main(String[] args)
+    {
+        println "--- starting Dialog ---"
+
+        Dialog obj;
+		if (args.size() > 0)
+		{
+			obj = new Dialog(args[1].toString());
+			println "... projectDir:"+args[1];
+		} // end of if
+		else
+		{
+	        obj = new Dialog();
+    	} // end of else
+    	    
+		obj.build();        
+        println "Dialog = [${obj.toString()}]"
+        println "--- the end of Dialog ---"
+    } // end of main
+
+} // end of class
---
+++ src/main/groovy/com/toolkit/tools/ProjectProperties.groovy
@@ -0,0 +1,487 @@
+package com.toolkit.tools;
+
+import groovy.transform.*;
+
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** 
+ * ProjectProperties class description
+ *
+ * This is code with all variable bits needed to do a project
+ *
+ */ 
+ @Canonical 
+ @groovy.beans.Bindable
+ public class ProjectProperties
+ {
+    /** an O/S specific char. as a file path divider */
+    String fs = java.io.File.separator;
+
+
+    /** an id of GitHub user who will host this project */
+    String gitHubUser = "martin";
+
+
+   /** 
+    * Variable full name of original author.
+    */  
+    String author = "Martin Quickman";
+
+   /** 
+    * Variable name of email for documentation purposes.
+    */  
+    String email = "martin.quickman@yahoo.co.uk";
+
+
+    /** copyright year of authorship */ 
+    String year = "2022";
+       
+
+   /** 
+    * Variable name of the actual folder for this project. Usually this is the projectDir folder name.
+    */  
+    String projectRoot = System.getProperty("user.dir");
+
+
+   /** 
+    * Variable name of this generated project. Usually this is the projectDir folder name.
+    */  
+    String projectName = "/Toolkit";
+
+
+   /** 
+    * Variable name of the actual folder for this project. Usually this is the projectDir folder name.
+    */  
+    String projectLocation = System.getProperty("user.dir");
+
+
+   /** 
+    * Hardware folder name hosting this project. Used to build folder hierarchy
+    * within xxx/src/main/groovy etc.
+    */  
+    String packageFolder = "/com/toolkit/tools";
+
+
+   /** 
+    * Variable value of package statement copied into jvm language source code.
+    */  
+    String packageName = "package com.toolkit.tools;";
+
+
+   /** 
+    * Software folder name hosting this project. Used in import statements
+    * mirrors packagefolder structure.
+    */  
+    String packagePrefix = "com.toolkit.tools.";
+
+   /** 
+    * Variable name of the class being built in the chosen jvm language. Also
+    * used to name the output script as Tools.java Tools.groovy. Can be changed
+    * from gradle command line using option -PprimaryClassName=xxx 
+    */  
+    String primaryClassName = "Tools";
+
+
+   /** 
+    * Variable name of a series of primaryClassName's being built in the chosen jvm language. Also
+    * used to name the output script as Tools.java Tools.groovy, etc. 
+    *
+    * Can be changed from gradle command line using option -Pclasses=xxx 
+    * Works just like 'primaryClassName' but for several sets of source files to be generated
+    *
+    * List may be quote or apostrophe delimited but must have commas between each class name
+    * so: Fred,'Hammer.java',  "Screws.scala" would generate 3 source files of 1)Fred.groovy 2)Hammer.java 3)Screws.scala
+	* Trailing suffix of primaryClassName is optional & defaults to .groovy
+    */  
+    String classes = "";
+
+
+   /** 
+    * Variable name of Map of all internal variables.
+    */  
+	Map map = [:]
+
+
+   /** 
+    * Default Constructor 
+    * 
+    * @return ProjectProperties object
+    */     
+    public ProjectProperties()
+    {
+        println "running ProjectProperties constructor"
+        this.load();
+        this.setProjectLocation();
+        primaryClassName = fixPrimaryClassName();
+        map = this.toMap();
+    } // end of constructor
+
+   /** 
+    * Non-Default Constructor 
+    * 
+    * @param projectDir has folder name of current project - NOT the parent folder name !
+    * @return ProjectProperties object
+    */     
+    public ProjectProperties(String projectDir)
+    {
+        println "running ProjectProperties non-dft constructor [${projectDir}]"
+        this.load();
+        projectRoot = projectDir;
+        this.setProjectLocation();
+        primaryClassName = fixPrimaryClassName();
+        map = this.toMap();
+    } // end of constructor
+
+
+   /** 
+    * Method to display internal variables.
+    * 
+    * @return formatted content of internal variables
+    */     
+    public String toString()
+    {
+        return """gitHubUser=${gitHubUser}
+author=${author}
+email=${email}
+year=${year}
+projectRoot=${projectRoot}
+projectName=${projectName}
+projectLocation=${projectLocation}
+packageFolder=${packageFolder}
+packageName=${packageName}
+packagePrefix=${packagePrefix}
+primaryClassName=${primaryClassName}
+classes=${classes}
+"""
+    }  // end of string
+
+
+   /** 
+    * Method to return a map variable.
+    * 
+    * @return formatted content of internal variables as a Map
+    */     
+    public Map toMap()
+    {
+    	def m = [:]
+        m["gitHubUser"]=gitHubUser
+        m["author"]=author
+        m["email"]=email
+		m["year"]=year
+
+        m["projectRoot"]=projectRoot
+		m["projectName"]=projectName
+        m["projectLocation"]=projectLocation
+
+        m["packageFolder"]=packageFolder    
+		m["packageName"]=packageName
+		m["packagePrefix"]=packagePrefix
+		m["primaryClassName"]=primaryClassName
+		m["classes"]=classes
+		return m;
+    }  // end of toMap
+
+   /** 
+    * Method to print audit log.
+    * 
+    * @param the text to be said
+    * @return void
+    */     
+    public void say(txt)
+    {
+        println txt;
+    }  // end of method
+
+
+   /** 
+    * Method to write all object's variables to persistent store named gradle.properties.
+    * 
+    * @return true if write to new output file was successful
+    */     
+    public boolean dump()
+    {
+        def file5 = new File('gradle.properties');
+        file5.withWriter('UTF-8') {
+                it.writeLine 'org.gradle.daemon=true'  
+                it.writeLine 'org.gradle.parallel=true'
+                it.writeLine this.toString();
+        } // end of writer
+
+        return true;        
+    } // end of load
+
+
+
+   /** 
+    * Method to read all Cell objects from persistent store into our list.
+    * 
+    * @param the Cells array to be loaded
+    * @return void
+    */     
+    public int load()
+    {
+        def file3 = new File('gradle.properties')
+
+        // Use a reader object:
+        int count = 0
+        def line
+        def txs 
+        def toks
+        file3.withReader { reader ->
+            while (line = reader.readLine()) {
+                txs = line.trim();
+                def i =  txs.indexOf("//");
+
+                if ( i > -1 )
+                {
+                    txs = txs.substring(0,i);
+                }
+
+                //println "... txs=["+txs+"]";
+                if (txs.trim().size() > 0 )
+                {
+                    // packagename =package com.toolbag.tools; 
+                    toks = txs.split('=')
+                    //println "... toks.size()="+toks.size();
+                    
+                    def t1 = toks[0].trim(); // typically, the key
+                    def t2 = (toks.size() > 1) ? toks[1].trim() : ""; // value
+					println "... token 0 =[$t1] and token 1 = [${t2}]"
+					
+					switch (t1)
+					{
+						case "gitHubUser": gitHubUser = t2; 
+							break;
+                        case "author": author = t2; 
+                            break;
+                        case "email": email = t2; 
+                            break;
+						case "year": year = t2; 
+							break;
+
+                        case "projectRoot": projectRoot = t2;
+                            break;                                  
+						case "projectName": projectName = t2; 
+							break;
+                        case "projectLocation": projectLocation = t2;
+                            break;                                  
+
+                        case "packageFolder": packageFolder = t2; 
+                            break;
+						case "packageName": packageName = t2; 
+							break;
+						case "packagePrefix": packagePrefix = t2; 
+							break;
+
+						case "primaryClassName": primaryClassName = t2; 
+							break;
+						case "classes": classes = t2; 
+							break;
+					} // end of switch
+					
+				} // end of if
+              } // end of while
+              
+        } // end of reader
+        
+        println "... loaded $count entries"
+        map = this.toMap();
+        return count;        
+    } // end of load
+    
+
+    
+   /** 
+    * Method to return a naked class name with no source file suffix like .groovy
+    * 
+    * @return String the class name with no trailing suffix like .java, .groovy, etc.
+	*/
+    public String fixPrimaryClassName() 
+    {
+    	int j = primaryClassName.indexOf('.');
+        print "\n... fixPrimaryClassName()=[${primaryClassName}] j=${j} "
+
+    	def t = (j < 0) ? primaryClassName : primaryClassName.substring(0,j) ;
+        println " now: t=[${t}]"
+        return t.toString();
+	} // end of method    
+
+    
+
+   /** 
+    * Method to return a piece of the projectDir excluding the TemplateProject folder. It
+    * then divides it into two var.s called: projectRoot + projectName
+    * 
+    * @return String the parent folder name above our own TemplateProject
+    */     
+    public String setProjectLocation() 
+    {
+    	int j = this.projectRoot.lastIndexOf(fs);
+        println "\n... projectRoot=[${projectRoot}] j=${j}"
+
+    	String t = (j < 0) ? projectRoot : projectRoot.substring(0,j) ;
+        println "... t=[${t}]"
+
+    	int k = t.lastIndexOf(fs);
+        println "... k=[${k}]"
+
+        if (k > -1)
+        {
+    	   projectName = projectRoot.substring(k+1,j); // this should be a single /foldername
+           println "... projectName set to=[${projectName}]"           
+    	   projectRoot = t.substring(0,k);
+           println "... projectRoot set to=[${projectRoot}]"           
+        } // end of if
+  
+        println "... setProjectLocation ended with projectRoot=${projectRoot} projectName=[${projectName}]"
+  
+        projectLocation = projectRoot + fs + projectName;
+    	return projectLocation;
+    } // end of setProjectDir
+
+
+   /** 
+    * Method to return a list of strings representing class names of sources to be built.
+    * 
+    * @return List true if write to new output file was successful
+    */     
+    public List getClassNames() 
+    {
+        def names = [];
+        def goodnames = [];
+        String token="";
+
+        def cn = classes.trim();
+        if (cn.size() > 0)
+        {
+	        names = cn.tokenize( ',' )
+	        names.each{e-> 
+	        	println "... name=[${e}]"
+	        	token = e;
+
+	        	def f = e.trim();
+	        	println "... f=[${f}]"
+	        	int i = f.indexOf(/"/)
+	        	if ( i > -1 ) 
+	        	{
+	        		int j = f.lastIndexOf(/"/)
+	        		println "... end quote at j="+j;
+	        		if ( j > i )
+	        		{
+	        			token = f.substring(i+1,j).trim();
+			        	println "... f=[${token}]"
+	        		} // end of if
+		        	else
+		        	{
+						token = f.substring(i+1).trim();
+			    		println "... f=[${token}]"
+		    		} // end of else
+	        	} // end of if
+	        	else
+	        	{
+	        		i = f.indexOf(/'/)
+		        	if ( i > -1 ) 
+		        	{
+		        		int k = f.lastIndexOf(/'/)
+		        		println "... end quote at k="+k;
+		        		if ( k > i )
+		        		{
+		        			token = f.substring(i+1,k).trim();
+			        		println "... f=[${token}]"
+		        		} // end  of if
+		        		else
+		        		{
+							token = f.substring(i+1).trim();
+			        		println "... f=[${token}]"
+		        		} // end of else
+		        	} // end  of if
+	        	}
+
+				token.replaceAll('"',' ')
+				token.replaceAll(/\'/,' ')
+				token = token.trim();
+
+				if (token.size()>0) 
+				{ 
+					int m = token.indexOf('.');
+					if (m < 1) token+=".groovy";  
+                    String na = token.toLowerCase();
+                    if (na.endsWith(".groovy") || na.endsWith(".java")  || na.endsWith(".scala" ))
+                    {
+                        goodnames += token;
+                    }  // end of if
+				} // end of if
+				
+	        	println "... token=[${token}]\n"
+	        } // end of each
+
+        } // end of if
+
+        return goodnames;        
+    } // end of get
+
+
+   // ======================================
+   /** 
+    * Method to run class tests.
+    * 
+    * @param args Value is string array - possibly empty - of command-line values. 
+    * @return void
+    */     
+    public static void main(String[] args)
+    {
+        println "-------- starting ProjectProperties ---------"
+	    ProjectProperties obj  = new ProjectProperties();
+        println "Default ProjectProperties = [" + obj.toString() + "]"
+	    println "---------------------------------------------"     
+
+        Map m = obj.toMap()
+        m.each{k,v-> println "... map[${k}]=[${v}]"}
+        println "---------------------------------------------"     
+
+        def x = obj.getClassNames();
+		x.each{y-> println "... create source file name="+y; }
+        println "---------------------------------------------"     
+		
+        // write updated version of gradle.properties with this TemplateProject folder
+        obj.dump();
+        println "---------------------------------------------"     
+
+        if (args.size() > 0)
+        {
+            println "---------------------------------------------"     
+            println "... create new object using directory :"+args[1];
+            obj = new ProjectProperties(args[1].toString());
+            println "---------------------------------------------"     
+            println "ProjectProperties = [" + obj.toString() + "]"
+            m = obj.toMap()
+            m.each{k,v-> println "... map[${k}]=[${v}]"}
+            println "---------------------------------------------"     
+
+            x = obj.getClassNames();
+            x.each{y-> println "... create source file name="+y; }
+            println "---------------------------------------------"     
+        
+            // write updated version of gradle.properties with this TemplateProject folder
+            obj.dump();
+            println "---------------------------------------------"     
+        } // end of if
+
+        println "--- the end of ProjectProperties ---"
+    } // end of main
+
+} // end of class
